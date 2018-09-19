b9087ad1856179f4839349b61483f61bd0957b03
mike <blsn2000@gmail.com>
2018-09-19 16:01:06 +0300

    dashboard allow

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,11 +28,12 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies($gate);
 
         //
-
+        /*
         $gate->define('isAuthor', function($user){
             return ($user->user_type == 'author') || ($user->user_type == 'admin');
         });
-
+        */
+        
         /*
         $gate->define('isAdmin', function($user){
             return $user->user_type == 'admin';
